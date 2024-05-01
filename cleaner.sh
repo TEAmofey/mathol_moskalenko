03d6f9da993e51decca00bb07b291286bd6c0542
teamofey <teamofey1982@gmail.com>
2024-05-02 01:47:58 +0300

    modified all

--- cleaner.sh
+++ cleaner.sh
@@ -1,2 +1,3 @@
 # shellcheck disable=SC2035
 rm -rf */*.(synctex.gz|log|fls|aux|fdb_latexmk|synctex|out|bbl|blg|pre|600gf|tfm)
+rm -rf */*/*.(synctex.gz|log|fls|aux|fdb_latexmk|synctex|out|bbl|blg|pre|600gf|tfm)
